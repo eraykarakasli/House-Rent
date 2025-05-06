f2a0cf796db964b2189c7ce941d59b633c193af5
Eray Karakaşlı <eraycode01@gmail.com>
2025-05-06 16:12:35 +0300

    Update index.php

--- index.php
+++ index.php
@@ -6,9 +6,7 @@ include "./tema/includes/header/header.php";
 
 <div class="container min-vh-100 mt-5">
     <?php include "./tema/home/searchbar/searchbar.php"; ?>
-   
     <?php include "./tema/home/homesliders/homeslider3.php"; ?>
-
 </div>
 
 <?php include "./tema/includes/footer/footer.php"; ?>
